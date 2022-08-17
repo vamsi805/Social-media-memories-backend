6574885129fb89b4c65c7029310abd38129e70af
vamsi805 <vamsibobba805@gmail.com>
2022-08-17 19:59:29 +0530

    New base url

--- index.js
+++ index.js
@@ -13,6 +13,7 @@ app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
 app.use(cors());
 
 app.use('/posts', postRoutes);
+
 app.get('/',(req,res)=>{
   res.send('Backend of memories project');
 })
